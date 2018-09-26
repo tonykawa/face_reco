52acf1c2ceb58b07f7c87ab5e53da087a80cb7ad
JC <jackcchau@gmail.com>
2018-09-27 00:35:43 +0800

    ADD main.py for implementing this project

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
-import face_recognition
-import os
-from params import common
 import File
+import FaceReco
 import datetime
 
-def face_reco(filename):
-    print(datetime.datetime.now())
-    knowPath = common.imagePath.known + filename
-    picture_of_me = face_recognition.load_image_file(knowPath)
-    my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+def main():
+    startTime = datetime.datetime.now()
+    for f in File.importKnownImages():
+        FaceReco.main(f)
 
-    filename_w_ext = os.path.basename(filename)
-    ppl_name, file_extension = os.path.splitext(filename_w_ext)
-    # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
+    endTime = datetime.datetime.now()
+    print("Total Time Usage:",endTime - startTime)
 
-    # list of file
-    for f in File.importUnknownImages():
-        if File.isImageFile(f) == False: continue
-
-        unknownPath = common.imagePath.unknown + f
-        print(unknownPath)
-        unknown_picture = face_recognition.load_image_file(unknownPath)
-        unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
-
-        # Now we can see the two face encodings are of the same person with `compare_faces`!
-        results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-
-        if results[0] == True:
-            print(f + " have \"" + ppl_name + "\" face!")
-            File.createFolder(filename, f, True)
-        else:
-            print(f + " have't \"" + ppl_name + "\" face!")
-
-        print(datetime.datetime.now())
-
-face_reco("ice.jpg")
+main()
